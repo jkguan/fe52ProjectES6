5ea604906eb09784989afb283029bf3f2d066410
jkguan <jacksonguan89@gmail.com>
2020-09-26 16:51:27 +0700

    Add function Delete

--- src/addUser.js
+++ src/addUser.js
@@ -1,8 +1,7 @@
 const addUser = () =>{
-<<<<<<< HEAD
     console.log("TUICUANHANH DA LAM XONG TASK!");
 };
-=======
-    console.log("DA LAM XONG TASK");
-;}
->>>>>>> master
+
+const deleteUser = () =>{
+    console.log("Xoa user");
+}
